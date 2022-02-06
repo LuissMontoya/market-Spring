e874e0eef9ec99be42d740f973204f9c9edb4239
Luiss Montoya <luisgoemon@gmail.com>
2022-02-05 19:38:40 -0500

    Data Mapper

---
+++ platzi-market/src/main/java/com/platzi/market/persistence/mapper/CategoryMapper.java
@@ -0,0 +1,26 @@
+package com.platzi.market.persistence.mapper;
+
+
+import com.platzi.market.domain.Category;
+import com.platzi.market.persistence.entity.Categoria;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+@Mapper(componentModel = "spring")
+public interface CategoryMapper {
+    @Mappings({
+            @Mapping(source = "idCategoria", target = "CategoryId"),
+            @Mapping(source = "descripcion", target = "Category"),
+            @Mapping(source = "estado", target = "active"),
+
+    })
+    Category toCategory(Categoria categoria);
+
+
+    @InheritInverseConfiguration
+    @Mapping(target = "productos", ignore = true)
+    Categoria toCategoria(Category category);
+
+}
