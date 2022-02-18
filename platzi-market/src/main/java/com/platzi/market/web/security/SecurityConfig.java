9fd74a04bb87e5f6ec57f949525d13ce47178dab
Luiss Montoya <luisgoemon@gmail.com>
2022-02-18 18:49:25 -0500

    JWT

--- platzi-market/src/main/java/com/platzi/market/web/security/SecurityConfig.java
+++ platzi-market/src/main/java/com/platzi/market/web/security/SecurityConfig.java
@@ -1,13 +1,23 @@
 package com.platzi.market.web.security;
 
 import com.platzi.market.domain.service.UserDetailService;
+import com.platzi.market.web.security.filter.JwtFlterRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private JwtFlterRequest jwtFilterRequest;
+
 
     @Autowired
     private UserDetailService userDetailService;
@@ -16,5 +26,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailService);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+       http.csrf().disable().authorizeRequests().antMatchers("/**/authenticate").permitAll()
+               .anyRequest().authenticated().and().sessionManagement()
+               .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+       http.addFilterBefore(jwtFilterRequest, UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
